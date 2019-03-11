a49fc1ec4bbdf257f3d35af235696998e88bd2fd
kaushik kasibhatla <kasibhatla.kaushik@sapient.com>
2019-03-11 16:51:35 +0530

    pushing initial setup

---
+++ back-end/src/main/java/com/kaushik/springbootreactpoc/SpringbootReactPocApplication.java
@@ -0,0 +1,12 @@
+package com.kaushik.springbootreactpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactPocApplication.class, args);
+	}
+}
---
+++ back-end/src/test/java/com/kaushik/springbootreactpoc/SpringbootReactPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaushik.springbootreactpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootReactPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
